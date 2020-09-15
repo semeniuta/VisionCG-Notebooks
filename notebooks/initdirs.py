d358381d1e01d78e0b3c4d86c13d4181a32363fe
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-09-15 15:46:19 +0200

    Update initdirs to match new names of libraries

--- notebooks/initdirs.py
+++ notebooks/initdirs.py
@@ -4,7 +4,7 @@ import os
 CODE_DIR = os.environ['CODEROOT']
 DATA_DIR = '../data'
 
-for module_dir in ('VisionCG', 'EPypes'):
+for module_dir in ('visionfuncs', 'EPypes', 'visiongraph'):
     m_path = os.path.join(CODE_DIR, module_dir)
     if m_path not in sys.path:
         sys.path.append(m_path)
